b21ef43048f0889221a4c3e0a76b8b44fa278c97
Jake Naydock <jacob.naydock@quadratec.com>
2024-02-07 21:23:08 -0500

    idk where to go from here my head hurts

--- ac3.2.js
+++ ac3.2.js
@@ -10,15 +10,6 @@ const fs = require('fs');
 const { default: test } = require('node:test');
 const data = fs.readFileSync('i3test.txt', 'utf-8');
 
-const numberTest = (arg) => {
-    if (!arg) return console.log(`${arg} is not a number and cannot be parsed`);
-    return arg.match(/\d/g) ? true : false;
-};
-
-const symbols = ['*', '%', '$', '@', '&', '+', '/', '#'];
-const isSymbol = (arg) => symbols.includes(arg);
-
-
 const isAst = arg => arg === '*';
 
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -27,16 +18,19 @@ const isNumber = (arg) => numbers.includes(arg);
 let allMatches = [];
 
 
-//Symbols are: * % $ @ & + / #
 let lines = data.split('\n');
-let partNumbers = [];
-let sum = 0;
+
 
 for (let line = 0; line < lines.length; line++) {
     let curLine = lines[line];
     let partCache = '';
     for (let i = 0; i < curLine.length; i++) {
         let currentChar = lines[line][i];
+
+        let curr2 = {
+            num: currentChar,
+            numIndex: lines[line][i]
+        }
         let twoBack = (i < 2) ? null : lines[line][i - 2];
         let previousChar = (i === 0) ? null : (lines[line][i - 1]);
         let nextChar = (i === lines[line].length - 1) ? null : (lines[line][i + 1]);
@@ -47,16 +41,16 @@ for (let line = 0; line < lines.length; line++) {
         let nextDiagLeft = (line === lines.length - 1) ? null : lines[line + 1][i - 1];
         let nextDiagRight = (line === lines.length - 1) ? null : lines[line + 1][i + 1];
 
-
-        let exactlyTwoMatches = false;
         let matchCache = [];
         let positions = [previousChar, nextChar, lastLineChar, nextLineChar, lastDiagLeft, lastDiagRight, nextDiagLeft, nextDiagRight];
 
 
+        //console.log(`Object keys prev char: ${Object.keys({ previousChar })[0]}`);
         if (isAst(currentChar)) {
             positions.forEach((pos) => {
                 if (isNumber(pos)) {
                     matchCache.push({
+                        varname: Object.keys({ pos })[0],
                         val: pos.toString(),
                         lineNumber: line,
                         charInd: i
@@ -80,6 +74,7 @@ console.log(`All matches: ${matchLog}`);
 
 let finalArray = [];
 let finalSum = 0;
+let match1Arr = [];
 
 for (let j = 0; j < allMatches.length; j++) {
     let partCache = '';
@@ -88,55 +83,21 @@ for (let j = 0; j < allMatches.length; j++) {
     let match1 = match[0];
     let match2 = match[1];
     console.log(`Full num index ${j} match 1: ${compileFullNumber(match1)}`);
-
-
-    //I need to find the numbers around the first matched number, then the second one.
-
-    /*
-    let currentChar = lines[match.lineNumber][match.charInd];
-    //console.log(`Current char in loop ${currentChar}`);
-    let twoBack = lines[match.lineNumber][match.charInd - 2];
-    let previousChar = lines[match.lineNumber][match.charInd - 1];
-    let nextChar = lines[match.lineNumber][match.charInd + 1];
-    let twoAhead = lines[match.lineNumber][match.charInd + 2];
-    console.log(`Two back in loop ${twoBack}`);
-
-    let sequence = []
-
-    if (isNumber(twoBack)) {
-        partCache += twoBack
-    } else if (isNumber()) {
-
-    }
-*/
+    let compiledMatch1 = compileFullNumber(match1);
+    match1Arr.push(compiledMatch1);
 }
 
+console.log(match1Arr);
 function compileFullNumber(matchObj) {
     let lineNo = matchObj.lineNumber;
     let ind = matchObj.charInd;
     let curLine = lines[lineNo];
 
-    /*
-        let positionsToCheck = [twoBack, prevChar, currentChar, nextChar, twoAhead];
-        positionsToCheck.forEach((pos) => { if (isNumber(pos)) sequence += pos });
-    
-       
-        //console.log(`Line no in function ${lineNo}`);
-        
-        let currentChar = lines[lineNo][ind];
-        //console.log(`Current char in loop ${currentChar}`);
-        let threeBack = lines[lineNo][ind - 3];
-        let twoBack = lines[lineNo][ind - 2];
-        let prevChar = lines[lineNo][ind - 1];
-        let nextChar = lines[lineNo][ind + 1];
-        let twoAhead = lines[lineNo][ind + 2];
-    */
-
     let sequence = '';
     for (let k = -3; k < 4; k++) {
         //console.log(`k in loop: ${k}`);
-        console.log(`Index: ${ind}`);
-        console.log(`Index plus k: ${ind + k}`)
+        //console.log(`Index: ${ind}`);
+        //console.log(`Index plus k: ${ind + k}`)
         let char = curLine[ind + k];
         //console.log(`Char in loop:${char}`);
         if (isNumber(char)) sequence += char;
@@ -144,7 +105,3 @@ function compileFullNumber(matchObj) {
 
     return sequence * 1;
 }
-
-//console.log(partNumbers);
-//console.log(`Sum: ${sum}`);
-
